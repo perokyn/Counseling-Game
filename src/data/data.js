d5127aa3a3612bdd753214631e3b04c0cefcd255
perokyn <perokyn@gmail.com>
2021-02-16 20:27:31 -0500

    added render logic and figure movement

---
+++ src/data/data.js
@@ -0,0 +1,273 @@
+export const data={"data":
+
+    [
+    
+
+        {"text":"hello",
+         "value":0,
+         "question":"how are you ?"
+        },
+
+        {"text":"haho",
+        "value":1,
+        "question":"are you happy?"
+       },
+
+       {"text":"yaaay",
+       "value":2,
+       "question":"Cats are fun?"
+        },
+        
+               
+        {"text":"super",
+        "value":3,
+        "question":"Cat and cat"
+         },
+         {"text":"super",
+         "value":4,
+         "question":"Cat and cat"
+          },
+
+
+
+          {"text":"hello",
+         "value":9,
+         "question":"how are you ?"
+        },
+
+        {"text":"haho",
+        "value":8,
+        "question":"are you ?"
+       },
+
+       {"text":"yaaay",
+       "value":7,
+       "question":"Cats are fun?"
+        },
+        
+               
+        {"text":"super",
+        "value":6,
+        "question":"Cat and cat"
+         },
+         {"text":"super",
+         "value":5,
+         "question":"Cat and cat"
+          },
+
+
+          {"text":"hello",
+          "value":10,
+          "question":"how are you ?"
+         },
+ 
+         {"text":"haho",
+         "value":11,
+         "question":"are you happy?"
+        },
+ 
+        {"text":"yaaay",
+        "value":12,
+        "question":"Cats are fun?"
+         },
+         
+                
+         {"text":"super",
+         "value":13,
+         "question":"Cat and cat"
+          },
+          {"text":"super",
+          "value":14,
+          "question":"Cat and cat"
+           },
+
+
+
+           {"text":"hello",
+           "value":19,
+           "question":"how are you ?"
+          },
+  
+          {"text":"haho",
+          "value":18,
+          "question":"are you happy?"
+         },
+  
+         {"text":"yaaay",
+         "value":17,
+         "question":"Cats are fun?"
+          },
+          
+                 
+          {"text":"super",
+          "value":16,
+          "question":"Cat and cat"
+           },
+           {"text":"super",
+           "value":15,
+           "question":"Cat and cat"
+            },
+              
+
+            {"text":"hello",
+            "value":20,
+            "question":"how are you ?"
+           },
+   
+           {"text":"haho",
+           "value":21,
+           "question":"are you happy?"
+          },
+   
+          {"text":"yaaay",
+          "value":22,
+          "question":"Cats are fun?"
+           },
+           
+                  
+           {"text":"super",
+           "value":23,
+           "question":"Cat and cat"
+            },
+            {"text":"super",
+            "value":24,
+            "question":"Cat and cat"
+             },
+
+             {"text":"hello",
+             "value":29,
+             "question":"how are you ?"
+            },
+    
+            {"text":"haho",
+            "value":28,
+            "question":"are you happy?"
+           },
+    
+           {"text":"yaaay",
+           "value":27,
+           "question":"Cats are fun?"
+            },
+            
+                   
+            {"text":"super",
+            "value":26,
+            "question":"Cat and cat"
+             },
+             {"text":"super",
+             "value":25,
+             "question":"Cat and cat"
+              },
+
+              {"text":"hello",
+              "value":30,
+              "question":"how are you ?"
+             },
+     
+             {"text":"haho",
+             "value":31,
+             "question":"are you happy?"
+            },
+     
+            {"text":"yaaay",
+            "value":32,
+            "question":"Cats are fun?"
+             },
+             
+                    
+             {"text":"super",
+             "value":33,
+             "question":"Cat and cat"
+              },
+              {"text":"super",
+              "value":34,
+              "question":"Cat and cat"
+               },
+
+               {"text":"hello",
+               "value":39,
+               "question":"how are you ?"
+              },
+      
+              {"text":"haho",
+              "value":38,
+              "question":"are you happy?"
+             },
+      
+             {"text":"yaaay",
+             "value":37,
+             "question":"Cats are fun?"
+              },
+              
+                     
+              {"text":"super",
+              "value":36,
+              "question":"Cat and cat"
+               },
+               {"text":"super",
+               "value":35,
+               "question":"Cat and cat"
+                },
+
+                {"text":"hello",
+                "value":40,
+                "question":"how are you ?"
+               },
+       
+               {"text":"haho",
+               "value":41,
+               "question":"are you happy?"
+              },
+       
+              {"text":"yaaay",
+              "value":42,
+              "question":"Cats are fun?"
+               },
+               
+                      
+               {"text":"super",
+               "value":43,
+               "question":"Cat and cat"
+                },
+                {"text":"super",
+                "value":44,
+                "question":"Cat and cat"
+                 },
+
+                 {"text":"hello",
+                 "value":49,
+                 "question":"how are you ?"
+                },
+        
+                {"text":"haho",
+                "value":48,
+                "question":"are you happy?"
+               },
+        
+               {"text":"yaaay",
+               "value":47,
+               "question":"Cats are fun?"
+                },
+                
+                       
+                {"text":"super",
+                "value":46,
+                "question":"Cat and cat"
+                 },
+                 {"text":"super",
+                 "value":45,
+                 "question":"Cat and cat"
+                  },
+      
+     
+    
+   
+  
+ 
+
+
+    ]
+
+
+
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,42 +1,14 @@
 import React, {useState} from 'react'
 import BaseStepSquare from '../components/BaseStepSquare'
 import $ from "jquery";
-
-
+import figure from '../assets/figure.png'
+import {data} from '../data/data'
 
 const MainPage=(props)=>{
 
 
     
-const content={"data":
-
-    [
-    
-
-        {"text":"hello",
-         "value":0,
-         "question":"how are you today?"
-        },
-
-        {"text":"haho",
-        "value":1,
-        "question":"are you happy?"
-       },
-
-       {"text":"yaaay",
-       "value":0,
-       "question":"Cats are fun?"
-        },
-        
-               
-
-
-
-    ]
-
-
-
-}
+const content=data
     
 ///get specific div step! TODO make this query in a loop and set innerhtml with timed functions
 $(function(){
@@ -47,21 +19,85 @@ $(function(){
     
     })
     
- 
+
+
+
+     
+console.log("datalength",content.data.length)
+ //check length of data and the number of times the interval runs, to keep figure visible on the last step
+  const setStep=()=>{
     
+    let time = 0;
+
+    let interval = setInterval(function() { 
+        if (time <= content.data.length-1) { 
+      
+            let  currentStep='#step'+time.toString()
+
+         
+            if(time>0){
+                let stepback='#step'+(time-1).toString()
+                $(function(){
+                    $(stepback).css('visibility', 'hidden' )
+            
+               })     
+            }
+
+            $(function(){
+                $(currentStep).css('visibility', 'visible')
+        
+           })
+
+          
+           time++;
+        }
+        else { 
+           clearInterval(interval);
+        }
+     }, 1000);
+   
+   
+       
+  }
+
+
+  setStep()
+ 
+
+
+const indexing=[0,1,2,3,4,9,8,7,6,5]
+    
+ // console.log("colors", setStep())
+  
 
 return(
 
 
 
-    <div className='flex' >
+    <div className='grid grid-cols-5 ' >
 
 
-{content.data.map((step,index)=>(
-    <div id={'step'+index.toString()} key={index}  >
-        {index}
-<BaseStepSquare  value={step.value}  content={step.text } question={step.question}/>
-</div>
+{content.data.map((step, index)=>(
+
+
+   
+   <div  key={step.value} className='relative ' >
+
+<div style={  step.value===4 ||step.value===14||step.value===24 || step.value===34 || step.value===44? 
+    {backgroundColor:'green', height:'2rem', borderTopRightRadius:'3rem', borderWidth:'2px',borderColor:'black',borderTopLeftRadius:'0.5rem',borderBottomLeftRadius:'0.2rem'} :{display:"hidden"}  }></div>
+
+    <div style={  step.value===9 || step.value===19 || step.value===29 || step.value===39 || step.value===49? 
+    {backgroundColor:'green', height:'2rem', borderTopLeftRadius:'3rem'} :{display:"hidden"}  }></div>
+
+
+
+     <img style={ { marginTop:step.value===4 ||step.value===14||step.value===24 || step.value===34 || step.value===44 || step.value===9 || step.value===19 || step.value===29 || step.value===39 || step.value===49? 
+        '2rem' : '0rem' } } id={'step'+step.value.toString()} alt='figure' src={figure}   className=' absolute -top-6 w-16 h-16 invisible'    ></img>    
+     
+     <div  >
+     <BaseStepSquare  value={step.value}  content={step.text } question={step.question}/>
+     </div>
+   </div>
 
 
 ))}
