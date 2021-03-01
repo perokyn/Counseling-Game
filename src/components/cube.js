7bd5c5e0477a4a89346b6741d3ae8c3df34e288b
perokyn <perokyn@gmail.com>
2021-03-01 10:33:34 -0500

    updating game logic

--- src/components/cube.js
+++ src/components/cube.js
@@ -1,72 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import $ from "jquery";
 import Emitter from '../utils/SpecialEvents'
 
-const Cube = React.forwardRef((props, ref)=>{
+const Cube = React.forwardRef((props, ref) => {
 
 
 
 
 
 
-let incr=1
+  let incr = 1
 
-    const roll= ()=> {
-      //console.log("startshow called",`show${incr}`)
-      
-      
-    
-        $('#cube').attr("class",`show${incr}`);
-    
-     
-       
-        if(incr===6) {
-          incr=1
-          
-        } else {
-            incr++
-          
-        }
-       
-      
-      
-    
-}
-        
-    
-
-
-  
+  const roll = () => {
+    //console.log("startshow called",`show${incr}`)
 
 
 
-      $(function() {
-        
-         let timing = 500;
-        let interval
-      
-        $("#start").click(function() {
-        
-          
-        interval = setInterval(function() {   
+    $('#cube').attr("class", `show${incr}`);
 
-          roll()
-  
-          },timing)
-         
-        });
-      
-        $("#stop").click(function() {
-     
-          clearInterval(interval);
-         
-          const className = $('#cube').attr('class');
-          console.log("current umber ",className ) 
-         Emitter.emit('ROLLED', className)
-          
-        });
-         
-      });
+
+
+    if (incr === 6) {
+      incr = 1
+
+    } else {
+      incr++
+
+    }
+
+
+
+
+  }
 
 
 
@@ -75,30 +40,65 @@ let incr=1
 
 
 
+  $(function () {
+
+    let timing = 500;
+    let interval
+
+    $("#start").click(function () {
+
+
+      interval = setInterval(function () {
+
+        roll()
+
+      }, timing)
+
+    });
+
+    $("#stop").click(function () {
+
+      clearInterval(interval);
+
+      const className = $('#cube').attr('class');
+      //console.log("current umber ",className ) 
+      Emitter.emit('ROLLED', className)
+
+    });
+
+  });
 
 
 
-    return (
 
-        <div ref={ref}  >
-            <div id="container">
-                <div id="cube" className="show1">
-                    <div className="top"></div>
-                    <div className="front"></div>
-                    <div className="left"></div>
-                    <div className="back"></div>
-                    <div className="right"></div>
-                    <div className="bottom"></div>
-                </div>
-                
+
+
+
+
+
+
+
+  return (
+
+    <div ref={ref}  >
+      <div id="container">
+        <div id="cube" className="show1">
+          <div className="top"></div>
+          <div className="front"></div>
+          <div className="left"></div>
+          <div className="back"></div>
+          <div className="right"></div>
+          <div className="bottom"></div>
         </div>
 
-        <div className='flex jsutify-between'>
-                    <button id='stop' className='bg-purple-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'> Stop Roll</button>
-                    <button id='start'  className='bg-green-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1' >Roll</button>
-                </div>
-        </div>
-    )
+      </div>
+
+      <div className='flex jsutify-between'>
+        <button id='stop' className='bg-purple-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'> Stop Roll</button>
+        <button id='start' className='bg-green-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1' >Roll</button>
+      </div>
+    </div>
+  )
 })
 
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,29 +8,43 @@ import Emitter from '../utils/SpecialEvents'
 const MainPage=(props)=>{
 
     const[currentRoll, setCurentRoll]=useState(0)
-    const[currentPosition, setCurentPosition]=useState(0)
+    const[currentPosition, setCurrentPosition]=useState(0)
+
+    const rollTo=useRef()
 
 
 useEffect(()=>{
 
-
-    Emitter.on("ROLLED", (value)=>{
-        if(!currentRoll){
-
-            setCurentRoll(parseInt(value.slice(4,5)) )
-
-        }else(setCurentRoll( parseInt(value.slice(4,5)) ))
-        })
-   
-   // console.log("colors", setStep())
-    
+   // console.log('CUBE REF',rollTo.current)
   
+  //  setCurentRoll( parseInt(rollTo.current.data.slice(4,5)) )
+  Emitter.on("ROLLED", (value)=>{
+       
+    
 
+    setCurentRoll( parseInt(value.slice(4,5)) )
+    
 
+    
+    
 })
+
+//setSteps()
+
+   console.log( 'ROLL FOMR EFFECT',currentRoll)
+
+//remember this wont fire if the rolled number is the same as currentRoll in the state!!!!
+   
+},[currentRoll])//whatever you put there, when it is changing it will re run the functions inside useeffect
+
+//console.log('CURRENT POSITION',currentRoll)
+//console.log('CURRENT ROLL',currentPosition)
+   
+
+
     
 const content=data
-const rollTo= React.createRef() 
+
 ///get specific div step! TODO make this query in a loop and set innerhtml with timed functions
 $(function(){
     $("#step2").click(function(){
@@ -41,23 +55,62 @@ $(function(){
     })
     
 
-
-
+ 
      
-console.log("datalength",content.data.length)
- //check length of data and the number of times the interval runs, to keep figure visible on the last step
-  const setStep=()=>{
-    
-    let time = 0;          ///<============this here can be the current position initialized to 0
 
+  /*  const setSteps=( )=>{
+//currentpiosition gets updated and keeps steps rolling to updated roll before new roll
+//onli update current position whem tehe is  a new roll.
+//if roll is less then current position , then current position wont get update due to the logic in setStep
+/*if(currentPosition>0 ){
+
+   // const updateRoll=currentRoll+currentPosition
+
+console.log("set Steps() called with",currentPosition,currentRoll)
+
+
+//setStep(currentPosition,updateRoll)
+}else{
+    
+    
+    console.log("else called")
+    
+//    setStep(currentPosition,currentRoll)
+}
+
+
+    }*/
+
+
+const afterRoll=(position)=>{
+
+//this below updates current posiiton but only if current roll is greater then
+    setCurrentPosition(currentPosition+position)
+         
+    console.log('CURRENT POS after roll:',currentPosition+position,)
+
+
+}
+
+
+//console.log("datalength",content.data.length)
+ //check length of data and the number of times the interval runs, to keep figure visible on the last step
+  const setStep=(position, goTo)=>{
+    
+   // let time = currentPosition;          ///<============this here can be the current position initialized to 0
+   //let goTo=currentRoll                   //Remember these variables were time =position before
+
+   
     let interval = setInterval(function() { 
-        if (time <= currentRoll) {             ///<=====set steps forward here   max step number=  content.data.length-1
+
+       
+        if (position <= goTo) {             ///<=====set steps forward here   max step number=  content.data.length-1
       
-            let  currentStep='#step'+time.toString()
+            let  currentStep='#step'+position.toString()
 
          
-            if(time>0){
-                let stepback='#step'+(time-1).toString()
+            if(position>0){
+                let stepback='#step'+(position-1).toString()
                 $(function(){
                     $(stepback).css('visibility', 'hidden' )
             
@@ -69,11 +122,17 @@ console.log("datalength",content.data.length)
         
            })
 
-          
-           time++;
+         
+           position++;
+         
+           if(position===currentRoll)
+        {
+         afterRoll(position)
         }
-        else { 
+
+        }else { 
            clearInterval(interval);
+          
         }
      }, 1000);
    
@@ -82,7 +141,7 @@ console.log("datalength",content.data.length)
   }
 
 
-  setStep()
+ //setSteps()
  
 
 
