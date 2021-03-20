1bd8f0d69f83a4e35551563d3b6d1cb7c5dac8f8
perokyn <perokyn@gmail.com>
2021-03-20 12:05:05 -0400

    added cube logic and button show/hide function

--- src/components/cube.js
+++ src/components/cube.js
@@ -1,36 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState , SetStateAction} from 'react'
 import $ from "jquery";
 import Emitter from '../utils/SpecialEvents'
 
 const Cube = React.forwardRef((props, ref) => {
 
 
-
-const[timesRolled, setTimesRolled]=useState(0)
-const[rollNUmber, setRollNumber]=useState(1)
-const[rolling, setRollin]=useState(true)
-
-
   
 
+const[showRoll, setShowRoll]=useState(false)
+const[showDice, setShowDice]=useState(false)
 
-
-
-
-
-
-
+const handleStopRoll=()=>{
+  setShowRoll(!showRoll)
+  setShowDice(!showDice)
+}
 
 
 
   return (
 
     <div ref={ref}  >
-      
+    
+    {showRoll &&
+    <p style={{color:'gray'}}>
+    {props.currentRoll}
+    </p>  
+    }
+{showDice ?
+    <p style={{color:'gray'}}>
+rolling dice
+    </p>  :<p></p>
+    }
+    
 
       <div className='flex jsutify-between'>
-        <button onMouseDown={props.onMouseDown} id='stop' className='bg-purple-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'> Stop Roll</button>
-        <button  id='start' className='bg-green-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'  onClick={props.onClick}>Roll</button>
+      {showDice &&
+    <button onMouseDown={props.onMouseDown} onClick={handleStopRoll} id='stop'
+    className='bg-purple-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'> 
+    Stop Roll
+    </button>  
+    }
+        
+       
+       {!showDice&& !showRoll&&
+       <button onMouseUp={()=>setTimeout(()=>{setShowDice(!showDice)},1000)}  id='start' 
+        className='bg-green-400 text-white text-sm font semibold rounded-xl p-3 mt-3 mx-1'  
+        onClick={props.onClick}>
+          Roll
+          </button>
+
+       
+       }
+        
+
       </div>
     </div>
   )
