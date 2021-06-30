313e68bb023b86244cadf0f7629a70837d4a275b
perokyn <perokyn@gmail.com>
2021-06-30 08:21:10 -0400

    formatted Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
 import MainPage from './MainPage'
 
-//-ytesting scaledrone
-import {TestDrone} from '../components/testDrone'//REMEMBER TO NOT CALL THIS AS A COMPONENt BUT EXPORT FUNcTIONS to avoid coNITNuOUS CONNECTING at each render!!
-import ConnectScaledrone from '../components/cscaledrone_connect'
-
-
-
 const HomePage = (props) => {
 
 return(
