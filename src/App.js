9717d8e38cd9031172a1406bd5131590fffad88e
perokyn <perokyn@gmail.com>
2022-01-20 20:33:50 -0500

    changed air table base

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const Airtable = require('airtable');
   Airtable.configure({
       endpointUrl: 'https://api.airtable.com',
-      apiKey: 'key7CMgeXJp3gxPkG'
+      apiKey: 'keyYnyKIu714D54nt'             //old key7CMgeXJp3gxPkG 
   });
   
 const base = require('airtable').base('appC9kYVpX4ewyzZM')
