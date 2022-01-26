15af03593336362f60fc985418d026252f13bf32
perokyn <perokyn@gmail.com>
2022-01-26 09:27:17 -0500

    formatted app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const Airtable = require('airtable');
   Airtable.configure({
       endpointUrl: 'https://api.airtable.com',
+      apiKey: 
   });
   
 const base = require('airtable').base('appC9kYVpX4ewyzZM')
