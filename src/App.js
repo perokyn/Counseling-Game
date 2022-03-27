6cdb973507b2d4d798b7c6b658a04aba15b2d2b9
perokyn <perokyn@gmail.com>
2022-03-27 10:50:57 -0400

    removed r key form app js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   
   const Airtable = require('airtable');
   Airtable.configure({
-      endpointUrl: 'https://api.airtable.com',
-      apiKey: 'keyYnyKIu714D54nt'             //old key7CMgeXJp3gxPkG 
+      endpointUrl: '',
+      apiKey: ''             //old key7CMgeXJp3gxPkG 
   });
   
 const base = require('airtable').base('appC9kYVpX4ewyzZM')
@@ -34,6 +34,8 @@ const location=window.location.href
   return (
     <div className="text-gray-300 text-4xl font-normal grid justify-items-center">
   {/* <TestDrone data={"hello Propss :)"}/> */}
+
+
 
   {/* animejs wont work in camel case section, no access to div */}
      <div className='flex mx-2  '>
