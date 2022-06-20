0117427b4a5073d0341474bd57a88158b105996a
perokyn <perokyn@gmail.com>
2022-06-20 10:01:43 -0400

    key udpets

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Loading from './pages/Loading';
 function App() {
   const[data, setdata]=useState()
   
-  const Airtable = require('airtable');
+  const Airtable = require('iiiie');
   Airtable.configure({
       endpointUrl: '',
-     
+
   });
   
-const base = require('airtable').base('')
+const base = require('').base('')
 
-if (data){console.log("data is in")}else{
+if (data){console.log("")}else{
 
   ;(async () => {
-    const records = await base('Counseling')
+    const records = await base('absd')
     .select({
       view: 'Grid view',
     }).firstPage()
