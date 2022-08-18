6695178de02a3ef945049b96a83b95489d9ec356
perokyn <perokyn@gmail.com>
2022-08-18 09:32:14 -0400

    formatted basetepsquare

--- src/components/BaseStepSquare.js
+++ src/components/BaseStepSquare.js
@@ -1,19 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const BaseStepSquare=React.forwardRef((props, ref)=>{
-    const randomColor =()=>{
-    const color=Math.floor(Math.random()*16777215).toString(16);
-    return "#" + color.toString();
-  }
-    const newColor=randomColor()
-return(
-    <div onClick={props.handleClick} className='flex p-3 border-2 border-gray-800 rounded-xl ' style={{backgroundColor:newColor}}>
-<div ref={props.ref}>
-<div className='text-sm font-bold text-white'>
-{props.question}
-</div>
-</div>
-    </div>
-)
+const BaseStepSquare = React.forwardRef((props, ref) => {
+    const randomColor = () => {
+        const color = Math.floor(Math.random() * 16777215).toString(16);
+        return "#" + color.toString();
+    }
+    const newColor = randomColor()
+    return (
+        <div onClick={props.handleClick} className='flex p-3 border-2 border-gray-800 rounded-xl ' style={{ backgroundColor: newColor }}>
+            <div ref={props.ref}>
+                <div className='text-sm font-bold text-white'>
+                    {props.question}
+                </div>
+            </div>
+        </div>
+    )
 })
 export default BaseStepSquare
